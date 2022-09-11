144063622a66e9bcde3fdd032cd480581bcd3b89
GitHub Action <action@github.com>
2022-09-11 20:50:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/liehann/modal/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/liehann/modal/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.liehann.modal.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.liehann.modal.MyBundle
 
 class MyApplicationService {
 
